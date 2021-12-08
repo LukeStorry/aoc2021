84398a78c493323ad03003590e433035e6cfbdea
Luke Storry <luke.storry@newtoneurope.com>
2021-12-08 17:23:28 +0000

    split out oneliner

--- day8.py
+++ day8.py
@@ -5,6 +5,7 @@ values = [[s.split() for s in line.split('|', 2)] for line in open("inputs/8.txt
 print(sum(1 for v in values for digit in v[1] if len(digit) in (2, 3, 4, 7)))
 
 
+# Part 2: Analysis-based mapping calculations
 def get_output(signals, outputs):
     mapping = {i: None for i in range(1, 10)}
     mapping[1] = next(set(signal) for signal in signals if len(signal) == 2)
@@ -20,10 +21,24 @@ def get_output(signals, outputs):
     return int(''.join(next(str(key) for key, char_set in mapping.items() if set(output) == char_set) for output in outputs))
 
 
-# Part 2: analysis-based mapping calculations
 print(sum(get_output(*v) for v in values))
 
-# Part 2: Brute-force oneliner
+
+# Part 2: Brute-force with sets
+sections = [set("abcefg"), set("cf"), set("acdeg"), set("acdfg"), set("bcdf"), set("abdfg"), set("abdefg"), set("acf"), set("abcdefg"), set("abcdfg")]
+
+apply_mapping = lambda mapping, digit: set(mapping["abcdefg".index(char)] for char in digit)
+
+get_mapping = lambda signal: next(possibility for possibility in permutations("abcdefg")
+                                     if all(apply_mapping(possibility, digit) in sections for digit in signal))
+
+decode_display = lambda mapping, output: int(''.join(str(value) for value in (sections.index(apply_mapping(mapping, digit)) for digit in output)))
+
+
+print(sum(decode_display(get_mapping(signal), output) for signal, output in values))
+
+
+# Part 2: Brute-force oneliner with strings instead of sets
 print(sum(next(int(''.join(str(x) for x in (["abcefg", "cf", "acdeg", "acdfg", "bcdf", "abdfg", "abdefg", "acf", "abcdefg", "abcdfg"].index(''.join(sorted(mapping["abcdefg".index(c)] for c in o))) for o in output)))
                for mapping in (possibility for possibility in permutations("abcdefg")
                                if all(''.join(sorted(possibility["abcdefg".index(char)] for char in digit)) in ["abcefg", "cf", "acdeg", "acdfg", "bcdf", "abdfg", "abdefg", "acf", "abcdefg", "abcdfg"] for digit in signal)))
