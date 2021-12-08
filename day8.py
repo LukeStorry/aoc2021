60cbda170689b00f46924659eeb3b8769c1cd35e
Luke Storry <luke.storry@newtoneurope.com>
2021-12-08 15:06:30 +0000

    Add oneline-bruteforce solution to 8p2

--- day8.py
+++ day8.py
@@ -1,6 +1,9 @@
+from itertools import permutations
 values = [[s.split() for s in line.split('|', 2)] for line in open("inputs/8.txt").read().splitlines()]
 
+# Part 1
 print(sum(1 for v in values for digit in v[1] if len(digit) in (2, 3, 4, 7)))
+
 
 def get_output(signals, outputs):
     mapping = {i: None for i in range(1, 10)}
@@ -16,4 +19,12 @@ def get_output(signals, outputs):
     mapping[0] = next(set(signal) for signal in signals if len(signal) == 6 and set(signal) not in mapping.values())
     return int(''.join(next(str(key) for key, char_set in mapping.items() if set(output) == char_set) for output in outputs))
 
+
+# Part 2: analysis-based mapping calculations
 print(sum(get_output(*v) for v in values))
+
+# Part 2: Brute-force oneliner
+print(sum(next(int(''.join(str(x) for x in (["abcefg", "cf", "acdeg", "acdfg", "bcdf", "abdfg", "abdefg", "acf", "abcdefg", "abcdfg"].index(''.join(sorted(mapping["abcdefg".index(c)] for c in o))) for o in output)))
+               for mapping in (possibility for possibility in permutations("abcdefg")
+                               if all(''.join(sorted(possibility["abcdefg".index(char)] for char in digit)) in ["abcefg", "cf", "acdeg", "acdfg", "bcdf", "abdfg", "abdefg", "acf", "abcdefg", "abcdfg"] for digit in signal)))
+          for signal, output in values))
