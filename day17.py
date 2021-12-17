af2ace5226d5a2d79903ad31bb0df431d61c9302
Luke Storry <luke.storry@newtoneurope.com>
2021-12-17 17:46:08 +0000

    day 17

--- day17.py
+++ day17.py
@@ -1,26 +1,25 @@
 from re import findall
 data = "target area: x=281..311, y=-74..-54"
-# data = "target area: x=20..30, y=-10..-5"
 xmin, xmax, ymin, ymax = (int(v) for v in findall(r"x=([-0-9]+)..([-0-9]+), y=([-0-9]+)..([-0-9]+)", data)[0])
 
 
 def check(xv, yv):
     x, y, highest = 0, 0, -1
-    while y > ymax:
+    while y >= ymin and x <= xmax:
+        if y <= ymax and x >= xmin:
+            return highest
         x += xv
         y += yv
-        xv += -1 if xv > 0 else 1
+        xv += 0 if xv == 0 else -1 if xv > 0 else 1
         yv -= 1
         highest = max(y, highest)
 
-    if y >= ymin and xmax >= x >= xmin:
-        return highest
-    return False
-
-best = 0
-for xv in range(-50,200):
-    for yv in range(-1,200):
-        best = max(check(xv,yv), best)
+    return None
 
 
-print(best)
+successes = [peak
+             for xv in range(5, 320)
+             for yv in range(-80, 500)
+             if (peak := check(xv, yv)) is not None]
+print(max(successes))
+print(len(successes))
